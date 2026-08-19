ade2fd1619278739e2373991974c82568245e20a
bhardwajprashant-1292008 <bhardwajprashant2008@gmail.com>
2026-08-19 09:14:56 +0530

    Add files via upload

---
+++ python_number_guessing_game_v1.py.py
@@ -0,0 +1,15 @@
+#Number Guess Game :->
+num=7
+guess=0
+while num!=guess:
+    guess=int(input("Enter your guess:"))
+    if guess==num:
+        print("congratulation's")
+    else:
+        print("try again")
+    choice=input("Do You Whant TO Continu Pls Enter (y/n)").lower()
+    if choice=="y":
+        continue
+    else:
+        print(Exiting)
+        break
